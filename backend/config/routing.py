271deb0b484817b4b724da9e8be8d4e435aa5d19
Hassan Kheireddin <hassankheireddin0@gmail.com>
2026-03-02 18:34:55 +0200

    add the apis

--- backend/config/routing.py
+++ backend/config/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from apps.games.matchmaking_consumer import MatchmakingConsumer
 from apps.games.pong_consumer import PongConsumer
 from apps.games.tictactoe_consumer import TicTacToeConsumer
+from apps.tournaments.tournament_consumer import TournamentConsumer
 
 websocket_urlpatterns: list = [
     re_path(
@@ -15,5 +16,9 @@ websocket_urlpatterns: list = [
     re_path(
         r"ws/matchmaking/$",
         MatchmakingConsumer.as_asgi(),
+    ),
+    re_path(
+        r"ws/tournament/(?P<tournament_id>[^/]+)/$",
+        TournamentConsumer.as_asgi(),
     ),
 ]
